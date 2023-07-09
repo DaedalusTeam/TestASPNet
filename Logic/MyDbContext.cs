21b81abeb23d055368b73bf8ca8623e2bbf57739
centuriin <centuriin@yandex.ru>
2023-07-09 17:08:38 +0300

    add db tests

---
+++ Logic/MyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Logic;
+
+public sealed class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+
+    public DbSet<SomeModel> SomeModels { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        CreateSomeModel(modelBuilder);
+    }
+
+    private void CreateSomeModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<SomeModel>()
+            .ToTable("some_model_table")
+            .HasKey(x => x.Id);
+    }
+}
---
+++ Logic/SomeModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Logic;
+public class SomeModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
